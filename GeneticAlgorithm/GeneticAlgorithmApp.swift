dadb28a35ac31984ab432306dde2743dcaa21fa5
carlneto <carlneto@icloud.com>
2024-09-08 18:55:43 +0100

    Initial Commit

---
+++ GeneticAlgorithm/GeneticAlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneticAlgorithmApp.swift
+//  GeneticAlgorithm
+//
+//  Created by Carlos Neto on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct GeneticAlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
